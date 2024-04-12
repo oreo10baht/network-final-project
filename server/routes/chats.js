fed4e8de20ae54d3f639c19b8b896b6d783d4155
Punnawatt <punnawathonglerdnapakul@gmail.com>
2024-04-13 01:48:19 +0700

    feat: chat models, controllers, routes

---
+++ server/routes/chats.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const chatController = require("../controllers/chatController");
+const router = express.Router();
+
+router.post("/", chatController.createChat);
+router.get("/:userId", chatController.findUserChats);
+router.get("/find/:firstId/:secondId", chatController.findChat); //change after using chatid
+
+module.exports = router;

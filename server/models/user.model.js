d27e8616889a6f55b14373056517549f2d576a2d
oreo10baht <papigonwin001@gmail.com>
2024-04-12 10:15:27 +0700

    feat: hash password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+SALT_WORK_FACTOR = 10;
 
 const UserSchema = mongoose.Schema(
   {
-    name: {
+    username: {
       type: String,
       required: [true, "Please enter your username."],
+      index: { unique: true },
     },
     password: {
       type: String,
@@ -19,6 +22,28 @@ const UserSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;

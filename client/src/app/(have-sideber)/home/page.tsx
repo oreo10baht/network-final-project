7c577c3bae1ea39afe1cdae90d48c01013bdec7e
WinSukon <jakrinsukon@gmail.com>
2024-04-13 13:11:14 +0700

    feat: add some init structure

---
+++ client/src/app/(have-sideber)/home/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/Sidebar";
+
+const page = () => {
+  return (
+    <div className="flex flex-col">
+      <Sidebar></Sidebar>
+    </div>
+  );
+};
+
+export default page;
---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,5 @@
+const Sidebar = () => {
+  return <div className="h-screen w-40 flex flex-col bg-slate-900">group</div>;
+};
+
+export default Sidebar;

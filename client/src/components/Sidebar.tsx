994b68a96b63b708924e34abb45e38c0de541936
WinSukon <jakrinsukon@gmail.com>
2024-04-13 13:18:01 +0700

    feat: added custom colors

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,5 +1,11 @@
 const Sidebar = () => {
-  return <div className="h-screen w-40 flex flex-col bg-slate-900">group</div>;
+  return <div className="h-screen w-40 fixed top-0 left-0 bg-slate-900 m-0 flex flex-col text-white ">
+    <i>a</i>
+    <i>s</i>
+    <i>s</i>
+    <i>f</i>
+
+  </div>;
 };
 
 export default Sidebar;

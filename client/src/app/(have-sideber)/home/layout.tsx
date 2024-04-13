54b8f8a5a1ffe99ca8c2d2b21460877ecae63d96
WinSukon <jakrinsukon@gmail.com>
2024-04-13 15:49:40 +0700

    feat: update style

--- client/src/app/(have-sideber)/home/layout.tsx
+++ client/src/app/(have-sideber)/home/layout.tsx
@@ -2,7 +2,7 @@ import HomeNavBar from "@/components/HomeNavBar";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-screen bg-gray-500">
+    <div className="w-screen bg-gray-700">
       <HomeNavBar></HomeNavBar>
       {children}
     </div>
--- client/src/components/HomeNavBar.tsx
+++ client/src/components/HomeNavBar.tsx
@@ -4,21 +4,21 @@ import { useRouter } from "next/navigation";
 const HomeNavBar = () => {
   const router = useRouter();
   return (
-    <div className="flex flex-row w-full h-14 bg-gray-700 justify-around items-center border-b-gray-950">
+    <div className="flex flex-row w-full h-14 bg-gray-700 justify-around items-center border-b-gray-950 border-b-2">
       <div
-        className="text-white rounded-lg px-2 py-1 hover:bg-gray-500 "
+        className="text-gray-400 font-medium rounded-lg px-2 py-1  hover:bg-gray-500 "
         onClick={() => router.push("/home/all")}
       >
         All
       </div>
       <div
-        className="text-white rounded-lg px-2 py-1 hover:bg-gray-500 "
+        className="text-gray-400 font-medium rounded-lg px-2 py-1 hover:bg-gray-500 "
         onClick={() => router.push("/home/online")}
       >
         Online
       </div>
       <div
-        className="text-white rounded-lg px-2 py-1 hover:bg-gray-500 "
+        className="text-gray-400 font-medium rounded-lg px-2 py-1 hover:bg-gray-500 "
         onClick={() => router.push("/home/pending")}
       >
         Pending

da50ccf75985427963f5117c296e602886ceef85
oreo10baht <papigonwin001@gmail.com>
2024-04-14 16:15:30 +0700

    fix:add user id to get user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,6 +28,7 @@ exports.getUserById = async (req, res) => {
       friends: user.friends,
       pendings: user.pendings,
       requests: user.requests,
+      user_id: user._id,
     };
     res.status(200).json(response);
   } catch (error) {
@@ -50,6 +51,7 @@ exports.getUserByName = async (req, res) => {
       friends: user.friends,
       pendings: user.pendings,
       requests: user.requests,
+      user_id: user._id,
     };
     res.status(200).json(response);
   } catch (error) {
@@ -69,6 +71,7 @@ exports.getAllUsers = async (req, res) => {
         friends: user.friends,
         pendings: user.pendings,
         requests: user.requests,
+        user_id: user._id,
       };
       return user;
     });

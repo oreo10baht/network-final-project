5611b819a6584f385c126fe1c90e3dec1e320aea
oreo10baht <papigonwin001@gmail.com>
2024-04-14 16:12:49 +0700

    feat: socket experimenting

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 const PORT = 8080;
 const connectDB = require("./db");
 
@@ -18,10 +27,14 @@ app.use("/api/users", usersRouter);
 app.use("/api/friends", friendsRouter);
 app.use("/api/chats", chatRoute);
 
-app.get("/api/home", (req, res) => {
-  res.json({ message: `Ok boys we're done.` });
+io.on("connection", (socket) => {
+  console.log("a user connected:", socket.id);
+  socket.on("send", (message) => {
+    socket.broadcast.emit("message", message);
+    // socket.broadcast.emit("message", socket.id);
+    console.log(message);
+  });
 });
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });

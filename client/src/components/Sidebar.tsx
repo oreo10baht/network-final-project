206cfdda81b34ef319712d464324777b6e6270b8
WinSukon <jakrinsukon@gmail.com>
2024-04-15 17:02:51 +0700

    feat: show group chats in sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { getGroupChats } from "@/services/Chats";
 import SidebarIcon from "./SidebarIcon";
 import { PlusIcon, HomeIcon } from "@radix-ui/react-icons";
 import { useState, useEffect } from "react";
-
+import { GetChat } from "@/models/Chat";
 
 const Sidebar = () => {
+  const [groupChats, setGroupChats] = useState<GetChat[]>([] as GetChat[]);
+  useEffect(() => {
+    const allGroupChat = async () => {
+      const gcs = await getGroupChats();
+      if (gcs) {
+        setGroupChats(gcs);
+      }
+    };
+    allGroupChat();
+  });
 
   return (
     <div className="overflow-auto no-scrollbar h-screen w-16 sticky bg-gray-900 top-0 left-0">
@@ -18,22 +29,14 @@ const Sidebar = () => {
         </SidebarIcon>
         <Divider></Divider>
 
-        {/* <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon>
-        <SidebarIcon text="GR"></SidebarIcon> */}
+        {groupChats
+          ? groupChats.map((groupChat: GetChat) => (
+              <SidebarIcon
+                text={groupChat.name!}
+                pathOnClicked={"/group/" + groupChat.chatId}
+              ></SidebarIcon>
+            ))
+          : null}
       </div>
     </div>
   );

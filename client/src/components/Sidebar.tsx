38938e0c72e634a409ae5e6e72774f59b8f5b1d2
WinSukon <jakrinsukon@gmail.com>
2024-04-15 16:44:23 +0700

    feat: form create group no api

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import SidebarIcon from "./SidebarIcon";
 import { PlusIcon, HomeIcon } from "@radix-ui/react-icons";
+import { useState, useEffect } from "react";
+
 
 const Sidebar = () => {
+
   return (
     <div className="overflow-auto no-scrollbar h-screen w-16 sticky bg-gray-900 top-0 left-0">
       <div className="  flex flex-col text-white  ">
         <SidebarIcon text="Home" pathOnClicked="/home/all">
           <HomeIcon className="size-6" />
         </SidebarIcon>
-        <SidebarIcon text="+" pathOnClicked="/home/all">
+        <SidebarIcon text="+" pathOnClicked="/group/create-group">
           <PlusIcon className="size-6" />
         </SidebarIcon>
         <Divider></Divider>

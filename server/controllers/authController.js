c5ef4a17d8d3a74f9089c46ff1ef4ef72f20993d
oreo10baht <111551012+oreo10baht@users.noreply.github.com>
2024-04-22 23:31:50 +0700

    fix: secure true

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,6 +24,7 @@ exports.userLogin = async (req, res) => {
           httpOnly: true,
           maxAge: 7 * 24 * 60 * 60 * 1000,
           sameSite: 'None',
+          secure: true
         });
         return res
           .status(200)

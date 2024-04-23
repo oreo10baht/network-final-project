1dbd30bc7fee2c7befc1a8623b06b098edf4d936
oreo10baht <111551012+oreo10baht@users.noreply.github.com>
2024-04-23 09:18:46 +0700

    fix: url

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -8,9 +8,6 @@ export const config = {
 export function middleware(request: NextRequest) {
   const res = request.cookies.get("token");
   if (!res) {
-    const currentURL = new URL(window.location.href);
-    const loginURL = new URL("/login", currentURL);
-    return NextResponse.redirect(loginURL);
-    // return NextResponse.redirect(new URL("http://localhost:3000/login"));
+    return NextResponse.redirect(new URL("https://clownfish-app-dyj8n.ondigitalocean.app/login"));
   }
 }

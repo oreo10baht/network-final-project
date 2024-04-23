5a4f031e10c8df0afcd536804d0a6c9bf7364488
oreo10baht <111551012+oreo10baht@users.noreply.github.com>
2024-04-23 22:03:37 +0700

    fix: revert auth back

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,8 +23,6 @@ exports.userLogin = async (req, res) => {
         res.cookie("token", token, {
           httpOnly: true,
           maxAge: 7 * 24 * 60 * 60 * 1000,
-          sameSite: "None",
-          // secure: true,
         });
         return res
           .status(200)
